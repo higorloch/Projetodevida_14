76891eee0fe15b3b6c363715fdae5e4137effd34
higorloch <higor.kotarski@escola.pr.gov.br>
2026-05-05 09:33:21 -0300

    ..

--- mine.js
+++ mine.js
@@ -1,17 +1,18 @@
-function calculaTempo(tempoObjetivo) {
-  let tempoAtual = new Date();
-  let tempoFinal = tempoObjetivo - tempoAtual;
-  let segundos = Math.floor(tempoFinal / 1000);
-  let minutos = Math.floor(segundos / 60);
-  let horas = Math.floor(minutos / 60);
-  let dias = Math.floor(horas / 24);
-
-  segundos %= 60;
-  minutos %= 60;
-  horas %= 24;
-  if (tempoFinal > 0) {
-    return [dias, horas, minutos, segundos];
-  } else {
-    return [0, 0, 0, 0];
-  }
-}
+<div class="contador">
+  <div class="contador-digito">
+    <p class="contador-digito-numero">7</p>
+    <p class="contador-digito-texto">dias</p>
+  </div>
+  <div class="contador-digito">
+    <p class="contador-digito-numero">7</p>
+    <p class="contador-digito-texto">horas</p>
+  </div>
+  <div class="contador-digito">
+    <p class="contador-digito-numero">7</p>
+    <p class="contador-digito-texto">min</p>
+  </div>
+  <div class="contador-digito">
+    <p class="contador-digito-numero">7</p>
+    <p class="contador-digito-texto">seg</p>
+  </div>
+</div>
